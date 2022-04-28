d45871d39fce7fd62a3723a7012c5ae35315f8e4
Alejandro Venegas <avenegasc14@gmail.com>
2022-04-28 10:18:04 -0600

    Add car skeleton

--- src/Car.js
+++ src/Car.js
@@ -1,19 +1,22 @@
-import * as THREE from "THREE";
+import * as THREE from "three";
 
-function Car(){
+
+const vehicleColors = [0xa52523, 0xbdb638, 0x78b141];
+
+export default function Car(){
     const car = new THREE.Group();
 
-    const backWheel = new THREE.Mesh(new THREE.BoxBufferGeometry(12, 33, 12), new THREE.MeshLambertMaterial({color: 0x333333}));
-    backWheel.position.z = 6;
-    backWheel.position.x = 18
+    const backWheel = Wheel();
+    backWheel.position.x = -18
+
     car.add(backWheel);
 
-    const frontWheel = new THREE.Mesh(new THREE.BoxBufferGeometry(12, 33, 12), new THREE.MeshLambertMaterial({color: 0x333333}));
-    frontWheel.position.z = 6;
+    const frontWheel = Wheel();
+
     frontWheel.position.x = 18
     car.add(frontWheel);
 
-    const main = new THREE.Mesh(new THREE.BoxBufferGeometry(30, 30, 15), new THREE.MeshLambertMaterial({color: 0xa52523}));
+    const main = new THREE.Mesh(new THREE.BoxBufferGeometry(60, 30, 15), new THREE.MeshLambertMaterial({color: 0xa52523}));
     main.position.z = 12;
     car.add(main);
 
@@ -22,14 +25,15 @@ function Car(){
     cabin.position.z = 25.5;
 
     car.add(cabin);
+
+    return car;
 }
 
+function Wheel(){
+    const wheel = new THREE.Mesh(new THREE.BoxBufferGeometry(12, 33, 12), new THREE.MeshLambertMaterial({color: 0x333333}));
+    wheel.position.z = 6;
 
-const scene = new THREE.Scene();
+    return wheel;
+}
 
-const playerCar = Car();
-scene.add(playerCar);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(ambientLight);
 
--- src/app.js
+++ src/app.js
@@ -8,3 +8,24 @@ scene.add(playerCar);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+dirLight.position.set(100, -300, 400)
+
+scene.add(dirLight);
+
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWith = 150;
+const cameraHeight = cameraWith / aspectRatio;
+
+const camera = new THREE.OrthographicCamera(cameraWith / -2, cameraWith / 2, cameraHeight / 2, cameraHeight / -2);
+
+camera.position.set(200, -200, 300);
+camera.up.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+document.body.appendChild(renderer.domElement);
